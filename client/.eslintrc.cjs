780a298d6ec9e98f68947823a626124d0b848b0c
Eivind Strand Harboe <eiha0403@gmail.com>
2024-02-14 15:47:08 +0100

    Fixed linting

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,43 +1,39 @@
 /** @type {import("eslint").Linter.Config} */
-const config = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-  ],
-  overrides: [
-        {
-         "files": ["*.ts", "*.tsx"],
-         "parser": "@typescript-eslint/parser"
-       }
-     ],
-  rules: {
-    // These opinionated rules are enabled in stylistic-type-checked above.
-    // Feel free to reconfigure them to your own preference.
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/require-await": "off",
-    "@typescript-eslint/no-misused-promises": [
-      "error",
-      {
-        checksVoidReturn: { attributes: false },
-      },
-    ],
-  },
-};
-
-module.exports = config;
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: './tsconfig.json',
+	},
+	plugins: ['@typescript-eslint', 'no-relative-import-paths'],
+	extends: [
+		'next/core-web-vitals',
+		'plugin:@typescript-eslint/recommended',
+	],
+	rules: {
+		'@typescript-eslint/consistent-type-imports': [
+			'warn',
+			{
+				prefer: 'type-imports',
+				fixStyle: 'inline-type-imports',
+			},
+		],
+		'@next/next/no-img-element': 'off',
+		'no-relative-import-paths/no-relative-import-paths': [
+			'warn',
+			{
+				prefix: '~',
+				rootDir: 'src',
+			},
+		],
+		'@typescript-eslint/no-misused-promises': [
+			'error',
+			{
+				checksVoidReturn: false,
+			},
+		],
+		'@typescript-eslint/no-empty-interface': 'off',
+		'@typescript-eslint/no-unused-vars': 'warn',
+		'@typescript-eslint/no-floating-promises': 'warn',
+		'@typescript-eslint/no-explicit-any': 'off',
+	},
+}
