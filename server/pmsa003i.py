f28ac92689aaf25295326b476e39d17da08eaa6f
Eivind Strand Harboe <eiha0403@gmail.com>
2024-03-13 13:06:02 +0000

    refactor backend

--- server/pmsa003i.py
+++ server/pmsa003i.py
@@ -1,10 +1,19 @@
-PMSA003I_ADDR = 0x12
+import time
+import board
+import busio
+from adafruit_pm25.i2c import PM25_I2C
 
-def read(bus):
-    um003 = bus.read_byte_data(PMSA003I_ADDR, 0x11) | bus.read_byte_data(PMSA003I_ADDR, 0x10) << 8
-    um005 = bus.read_byte_data(PMSA003I_ADDR, 0x13) | bus.read_byte_data(PMSA003I_ADDR, 0x12) << 8
-    um010 = bus.read_byte_data(PMSA003I_ADDR, 0x15) | bus.read_byte_data(PMSA003I_ADDR, 0x14) << 8
-    um025 = bus.read_byte_data(PMSA003I_ADDR, 0x17) | bus.read_byte_data(PMSA003I_ADDR, 0x16) << 8
-    um050 = bus.read_byte_data(PMSA003I_ADDR, 0x19) | bus.read_byte_data(PMSA003I_ADDR, 0x18) << 8
-    um100 = bus.read_byte_data(PMSA003I_ADDR, 0x1b) | bus.read_byte_data(PMSA003I_ADDR, 0x1a) << 8
-    return (um003, um005, um010, um025, um050, um100)
+# Create library object, use 'slow' 100KHz frequency!
+i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
+# Connect to a PM2.5 sensor over I2C
+reset_pin = None # Optional, set to None if unused
+pm25 = PM25_I2C(i2c, reset_pin)
+
+def read():
+    while True:
+        try:
+            aqdata = pm25.read()
+            return aqdata
+        except RuntimeError:
+            print("Unable to read from sensor, retrying...")
+        time.sleep(1)
